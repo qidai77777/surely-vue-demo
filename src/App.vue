927b2ef74a111acf2d18cd1d509d623eea294cac
qidai <43577925+qidai77777@users.noreply.github.com>
2024-05-20 06:59:51 +0000

    测试提交

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,77 @@
 <script setup>
-import {reactive} from "vue";
+import { reactive } from "vue";
 import Image from "./Image.vue";
 
 const dataList = reactive([
   {
-    "id":1,
-    "img": {
-      "path": "https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg"
+    id: 1,
+    img: {
+      path: "https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg",
     },
-    "bakImg": [
+    bakImg: [
       {
-        "path": "https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg"
+        path: "https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg",
       },
       {
-        "path": "https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg"
-      }
-    ]
+        path: "https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg",
+      },
+    ],
   },
   {
-    "id":2,
-    "img": {
-      "path": "https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg"
+    id: 2,
+    img: {
+      path: "https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg",
     },
-    "bakImg": [
+    bakImg: [
       {
-        "path": "https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg"
+        path: "https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg",
       },
       {
-        "path": "https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg"
+        path: "https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg",
       },
       {
-        "path": "https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg"
-      }
-    ]
-  }
-])
+        path: "https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg",
+      },
+    ],
+  },
+]);
 
 const columns = reactive([
   {
-    title: '序号',
-    key: 'id',
-    dataIndex: 'id',
+    title: "序号",
+    key: "id",
+    dataIndex: "id",
     width: 60,
   },
   {
-    title: '图片',
-    key: 'img',
-    dataIndex: 'img',
+    title: "图片",
+    key: "img",
+    dataIndex: "img",
     width: 250,
     autoHeight: true,
-    align: 'center',
+    align: "center",
     resizable: false,
   },
   {
-    title: '待选图片集',
-    key: 'bakImg',
-    dataIndex: 'bakImg',
+    title: "待选图片集",
+    key: "bakImg",
+    dataIndex: "bakImg",
     autoHeight: false,
     width: 250,
   },
-])
+]);
 </script>
 
 <template>
   <div class="table">
     <s-table
-        :columns="columns"
-        :pagination="false"
-        :data-source="dataList"
-        :deep-watch-data-source="true"
-        :row-key="record => record.id"
-        :locale="{ emptyText: '暂无数据' }"
-        stripe
+      :columns="columns"
+      :pagination="false"
+      :data-source="dataList"
+      :deep-watch-data-source="true"
+      :row-key="(record) => record.id"
+      :locale="{ emptyText: '暂无数据' }"
+      stripe
     >
       <template #bodyCell="{ record, index, column }">
         <!-- 图片 -->
@@ -86,9 +86,9 @@ const columns = reactive([
           <div class="image-bak-column">
             <el-scrollbar>
               <Image
-                  v-for="(item, subIndex) in record.bakImg"
-                  :key="item.id"
-                  :path="item.path"
+                v-for="(item, subIndex) in record.bakImg"
+                :key="item.id"
+                :path="item.path"
               />
             </el-scrollbar>
           </div>
